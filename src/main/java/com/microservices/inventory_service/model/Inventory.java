b76a45d91f635e7ee97016446e38615761f75202
nikita <nikita.goutor@gmail.com>
2024-12-03 23:23:09 +0300

    Update.

---
+++ src/main/java/com/microservices/inventory_service/model/Inventory.java
@@ -0,0 +1,7 @@
+package com.microservices.inventory_service.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Inventory {
+}
---
+++ src/main/resources/db/migration/V2__add_iventory.sql
@@ -0,0 +1,3 @@
+insert into t_inventory(quantity, sku_code)
+values (100, 'Iphone 15'),
+       (100, 'test');
